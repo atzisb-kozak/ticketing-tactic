4cdb6a02c4210df7e406192ce881725c91ee475f
atzisb-kozak <alkergoat@gmail.com>
2021-06-14 15:45:03 +0200

    [Feat](user) add user controller + service

--- ticketing-api/src/app.module.ts
+++ ticketing-api/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { set } from 'mongoose'
+
+set('useFindAndModify', false);
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [MongooseModule.forRootAsync({
+		useFactory: () => ({
+			uri: 'mongodb://root:toor@db:27017/ticketing?authSource=admin',
+			
+		})
+	}), UserModule],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
